1d325b0fa0ef09e8af6a41466bbf91f4931fe2a6
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-11 17:31:29 +0300

    changed auth() to auth('api')

--- laravel-server/app/Http/Controllers/JWTController.php
+++ laravel-server/app/Http/Controllers/JWTController.php
@@ -65,7 +65,7 @@ class JWTController extends Controller
             return response()->json($validator->errors(), 422);
         }
 
-        if (!$token = auth()->attempt($validator->validated())) {
+        if (!$token = auth('api')->attempt($validator->validated())) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
@@ -116,7 +116,8 @@ class JWTController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth('api')->factory()->getTTL() * 60, 
+            'user' => auth('api') -> user(),
         ]);
     }
 }
