f3ed6dbde1dd9cdaf3b3ec26dfe6cb7015355eec
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-13 07:27:13 +0300

    added login axios request

--- electron-admin/src/scripts/login.js
+++ electron-admin/src/scripts/login.js
@@ -7,16 +7,35 @@ window.onload = function () {
   login_btn.addEventListener("click", login);
 
   function login() {
-      if (checkFields())
-          alert("all good");
-      else
-          alert("Please fill all fields");   
+    if (checkFields())
+      loginRequest();
+    else
+      alert("Please fill all fields");
   }
 
   function checkFields() {
-      if (email.value != "" && password.value != "")
-          return true;
-      return false;
+    if (email.value != "" && password.value != "")
+      return true;
+    return false;
+  }
+
+  function loginRequest() {
+    let data = new FormData();
+    data.append('email', email.value);
+    data.append('password', password.value);
+    axios({
+      method: 'post',
+      url: 'http://localhost:8000/api/login',
+      data: data,
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+        alert("Invalid Username or Password")
+      });
   }
 
 }
